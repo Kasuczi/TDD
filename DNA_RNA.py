70d7ce80e1da3e186f6e24b5690b2ed3c2c1ddc0
Kasuczi <55882684+Kasuczi@users.noreply.github.com>
2019-10-13 13:44:47 +0200

    Add files via upload

--- DNA_RNA.py
+++ DNA_RNA.py
@@ -13,6 +13,15 @@ def hamming_metric(dna_c, dna_f):
     for c, f in zip(dna_c, dna_f):
         if c == f:
             metric_value += 1
-        return metric_value
+    return metric_value
 
+def i_am_your_father(fathers, child):
+    f_list = []
+    for key, father in fathers:
+        f_list.append(hamming_metric((father, child)))
+    if f_list.count(min(f_list)) > 1:
+        raise ValueError('Somebody is chetaing')
+    return f_list.index(min(f_list))
 
+if __name__ == '__main__':
+    pass
--- DNA_RNA_TEST.py
+++ DNA_RNA_TEST.py
@@ -4,7 +4,7 @@ Test for hamming_metric
 
 import unittest
 
-from src.DNA_RNA import hamming_metric
+from src.DNA_RNA import hamming_metric, i_am_your_father
 
 class testHamming(unittest.TestCase):
 
@@ -12,6 +12,10 @@ class testHamming(unittest.TestCase):
         testDna1 = 'GCTAC'
         testDna2 = 'CCTAT'
         hamming_metric(testDna1, testDna2)
-        self.assertEqual(hamming_metric(testDna1, testDna2), 0)
+        self.assertEqual(hamming_metric(testDna1, testDna2), 3)
 
 
+    def testFather(self):
+        fathers = ['ATCAG', 'GCTAT', 'GTCCG']
+        child = ['GCTAT']
+        self.assertEqual(i_am_your_father(fathers, child),0)
